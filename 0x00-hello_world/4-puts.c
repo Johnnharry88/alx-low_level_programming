60f9aad56be00b6e767337966df9ad1880bf11a2
Johnnharry88 <harryjohnwhye@gmail.com>
2023-08-10 20:56:53 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (void)
+{
+	// storing strings in A
+	char A[] ="Programming is like building a multilingual puzzle";
+	//routing to standard output	
+	puts(A);
+return 0;
+}
+
