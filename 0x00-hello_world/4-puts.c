80554cfc80b173353589cb3b8fee88f86a9fae8b
Johnnharry88 <harryjohnwhye@gmail.com>
2023-08-13 19:18:45 -0700

    task2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+/**
+ * main-Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main (void)
 {
-	// storing strings in A
-	char A[] ="Programming is like building a multilingual puzzle";
+	puts ("\"Programming is like building a multilingual puzzle");
 	//routing to standard output	
-	puts(A);
-return 0;
+return (0);
 }
 
