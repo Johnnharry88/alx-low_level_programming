374fe6897be7114a3f24f4877bd7300a3bfc8358
Johnnharry88 <harryjohnwhye@gmail.com>
2023-08-13 19:46:05 -0700

    Tash3

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,12 @@
 #include<stdio.h>
-int main (void)
-{	// using the printf
-
-	pritnf ('Alx is so cool!'};
-return 0;
+/**
+ * main-Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+return (0);
 }
 
