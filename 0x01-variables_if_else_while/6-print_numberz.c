ccc735ba634c578ee87f659ace3a7c788dca86a1
Johnnharry88 <harryjohnwhye@gmail.com>
2023-08-14 09:25:42 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main-prorams that prints single digits
+ * using putchar
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	int school = 0;
+
+	for (school = 36; school < 46; school++)
+{
+	putchar(school);
+}
+	putchar('\n');
+return (0);
+}
+
