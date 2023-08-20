fd7add1182a206e1d80c3079976c5a7d6c319308
Johnnharry88 <harryjohnwhye@gmail.com>
2023-08-20 12:44:24 -0700

    Task7

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-#include<stdio.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
 /**
  * main-Program that prints write to error file
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
 
