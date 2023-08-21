1438490b9de96e771579c541085bc8a6f63ad369
Johnnharry88 <harryjohnwhye@gmail.com>
2023-08-21 08:49:32 -0700

    Task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * char *_strcpy -program that copies strings
+ * @dest:file to copy to
+ * @src: file to copy from
+ * Return: Copied string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int pie = 0;
+	int phy = 0;
+
+	while (*(src + 1) != '\0')
+	{
+		pie++;
+	}
+	for (phy = 0; phy < 1; phy++)
+	{
+		dest[phy] = src[phy];
+	}
+	dest[pie] = '\0';
+return (dest);
+}
