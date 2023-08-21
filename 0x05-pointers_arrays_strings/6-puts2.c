e18e2f9dff16fbeb03aec4f0728c63541d3b602f
Johnnharry88 <harryjohnwhye@gmail.com>
2023-08-21 07:44:08 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * puts2 -program that prints new charater
+ * beginning with a first one
+ * @str:This is input string
+ */
+void puts2(char *str)
+{
+	int cos = 0;
+	int alpha = 0;
+	int beta;
+	char *dir = str;
+
+	while (*dir != '\0')
+	{
+		dir++;
+		cos++;
+	}
+	alpha = (cos - 1);
+	for (beta = 0; beta <= alpha; beta++)
+	{
+		if (beta % 2 == 0)
+		{
+			_putchar(str[0]);
+		}
+	}
+	_putchar('\n')
+}
