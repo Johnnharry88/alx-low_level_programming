c250170c16537ae230a91f68f167b5098816ff2e
Johnnharry88 <harryjohnwhye@gmail.com>
2023-08-21 10:01:24 -0700

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,3 +1,18 @@
 #include "main.h"
 /**
- *
+ * puts_half - program that prints half of a string
+ * @str: input string
+ * Return: Zero
+ */
+void puts_half(char *str)
+{
+	int alx;
+
+	for (alx = 0; str[alx] != '\0'; alx++)
+	alx++;
+	for (alx /= 2; str[alx] != '\0'; alx++)
+	{
+		_putchar(str[alx]);
+	}
+	_putchar('\n');
+}
