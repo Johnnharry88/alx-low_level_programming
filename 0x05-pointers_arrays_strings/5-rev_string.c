233baffec14db4493fc09db6cddbbc7515ff5dc5
Johnnharry88 <harryjohnwhye@gmail.com>
2023-08-21 07:15:45 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_strig -program that reverses strings
+ * @s: input string
+ * Return: Reversed string
+ */
+void rev_string(char *s)
+{
+	char inv = s[0];
+	int rec = 0;
+	int digits;
+
+	while (s[rec] != '\0')
+	rec++;
+	for (digits = 0; digits < rec; digits++)
+	{
+		rec--;
+		inv = s[digits];
+		s[digit] = s[rec];
+		s[rec] = inv;
+	}
+}
