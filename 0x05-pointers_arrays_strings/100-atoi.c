1156771f9ac6169d4789dc12299ed91c06331257
Johnnharry88 <harryjohnwhye@gmail.com>
2023-08-22 04:04:58 -0700

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,3 +1,33 @@
 #include "main.h"
 /**
- *
+ * _atoi - program that converts strings to int
+ * @s: Input string
+ * Return : Interger value
+ */
+int _atoi(char *s)
+{
+	unsigned int rec, x, o, p, a, i;
+	rec = 0;
+	x = 0;
+	o = 0;
+	p = 1;
+	a = 1;
+
+	while(*(s + rec) != '\0')
+	{
+		if(x > 0 && (*(s + rec) < '0' || *(s + rec) > '9'))
+		break;
+		if ((*(s + rec) >= '0') && (*(s + rec) <= '9'))
+		{
+			if (x > 0)
+			a* = 10;
+		}
+		rec++;
+	}
+	for (i = rec - x; i < rec; i++)
+	{
+		o = o + ((*(s + i) - 48) * a);
+		a /= 10;
+	}
+	return (o * p)
+}
