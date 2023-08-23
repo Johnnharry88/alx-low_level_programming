df939e59e52449b3b766668a9479a1a290c69cc1
Johnnharry88 <harryjohnwhye@gmail.com>
2023-08-23 07:36:46 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcat - program that concatenates two strings
+ * @dest: final string
+ * @src: Initiation string
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int string = 0;
+	int g = 0;
+
+	while (dest[string] != '\0')
+	{
+		string++;
+	}
+	for (g = 0; src[g] != '\0'; g++; string++)
+	{
+		dest[string] = src[g];
+	}
+	dest[string] = '\0';
+	return (dest);
+}
