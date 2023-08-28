04a1664b01af315e2ba6b0a52fb5b7673fb303f8
Johnnharry88 <harryjohnwhye@gmail.com>
2023-08-28 13:45:21 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,12 +9,10 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	int alx;
 
-		alx = 0;
-	while (n > 0)
+	for (alx = 0; n > 0; alx++)
 	{
 		s[alx] = b;
 		n = n - 1;
-		alx += 1;
 	}
 return (s);
 }
