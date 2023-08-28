a398c48e4296302f1aa0e098e9b98bbfaf0dfaf1
Johnnharry88 <harryjohnwhye@gmail.com>
2023-08-28 15:18:23 -0700

    TAsk 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strchr - progran that looks for a character in a string
+ * @s: Input string pointer
+ * @c: input character
+ * Return: (0)
+ */
+char *_strchr(char *s, char c)
+{
+	int alx = 0; 
+
+	while (s[alx] >= '\0')
+	{
+		if (s[alx] == c)
+		{
+			retur(s[alx]);
+		}
+	alx = alx + 1;
+	}
+return (0);
+}
