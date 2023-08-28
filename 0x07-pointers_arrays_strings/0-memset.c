a8d62c7f6e7ae287c0a3c1a1231c1648eb98563d
Johnnharry88 <harryjohnwhye@gmail.com>
2023-08-28 13:18:34 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,7 +3,7 @@
  * _memset -A function that fills a block of memory with a specific value
  * @s: address of empty memory
  * @b: Value to be inputed in memory
- * @n: byte number
+ * @n: number of byte
  */
 char *_memset(char *s, char b, unsigned int n)
 {
