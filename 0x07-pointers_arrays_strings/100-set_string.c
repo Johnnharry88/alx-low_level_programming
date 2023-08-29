b2d049fd43ab34760f9cea3d24043286a03076d2
Johnnharry88 <harryjohnwhye@gmail.com>
2023-08-28 20:53:44 -0700

    Yask 9

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * set_string -program that sets value of pointer to character
+ * @s: Special double  pointer
+ * to: Character pointer
+ * Return: (0)
+ */
+void set_string(char **s, char *t)
+{
+	*s = to;
+return (0);
+}
