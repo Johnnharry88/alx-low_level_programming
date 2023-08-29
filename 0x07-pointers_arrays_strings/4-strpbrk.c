d6d12a9af4b05b62f8d141ec1032aa0d812da697
Johnnharry88 <harryjohnwhye@gmail.com>
2023-08-28 18:34:34 -0700

    Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,21 @@
+#include "mian.h"
+/**
+ * _strpbrk - program that looks for a string for any of a set byte
+ * @s: Input character pointer
+ * @accept: Input character pointer
+ * Return: Null atring
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int alx = 0;
+
+	for (; accept[alx]; alx++)
+	{
+		if (*s == accept[alx])
+		{
+			return (s);
+		}
+	s += 1;
+	}
+return ('\0');
+}
