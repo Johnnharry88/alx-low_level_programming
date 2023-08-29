bdcaefbf6c0f27e8066db39f9a39aba92a05f6f4
Johnnharry88 <harryjohnwhye@gmail.com>
2023-08-28 20:17:15 -0700

    Task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_chessboard - program that prints chessboard 
+ * @a: Array of eight elements
+ * Return: (0) Success
+ */
+void print_chessboard(char (*a)[8])
+{
+	int alx, school
+
+	while (alx <=7)
+	{
+		while (school <= 7)
+		{
+			_putchar(a[alx][school]);
+			_putchar('\n');
+			school = school + 1;
+		}
+	alx = alx + 1;
+	}
+}
