cb366e38616823a4733b9635a7385bfd3fa59a30
Johnnharry88 <harryjohnwhye@gmail.com>
2023-08-28 17:12:38 -0700

    Task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *_strspn -program that prints gets the length of a substring prefix
+ *@s: Input pinter s
+ *@accept: Input pointer accept
+ *Return: (0)
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int school = 0;
+	int alx = 0;
+
+	while (*s)
+	{
+		while (accept[school])
+		{
+			if (*s == accept[alx])
+			{
+				alx = alx + 1;
+				break;
+			}
+			else if (accept[alx + 1] == '\0')
+			{
+				return (n);
+			}
+			alx = alx + 1;
+		}
+	return (n);
+}
