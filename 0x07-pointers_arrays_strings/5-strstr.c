38d4589cb04a1d36fc6cda973f85cee6e4b619ba
Johnnharry88 <harryjohnwhye@gmail.com>
2023-08-28 19:45:21 -0700

    TAsk 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strstr - a program that loactes a string
+ * @haystack: input pointer
+ * @needle: input pointer
+ * Return: void
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		char *alx = haystack;
+		char *school = needle;
+
+		while (*alx == *school && *shcool != '\0')
+		{
+			alx = alx + 1;
+			school = school + 1;
+		}
+		if (*school == '\0')
+		return (haystack);
+	}
+}
