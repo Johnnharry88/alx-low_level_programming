21be9dba0169038228f2893eb28d05598e067dc4
Johnnharry88 <harryjohnwhye@gmail.com>
2023-08-29 14:46:23 -0700

    Task 8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -15,7 +15,7 @@ void print_diagsums(int *a, int size)
 		alx1 = (alx + a(school * size + school));
 		school = school + 1;
 	}
-	while (school = size - 1 && school > -1)
+	while (school = size - 1 && school >= 0)
 	{
 		alx2 += a(school * size + (size - school -1));
 	}
