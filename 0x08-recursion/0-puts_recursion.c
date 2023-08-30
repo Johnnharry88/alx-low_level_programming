d45ef6deb5973b9fa2ae248876582f8155099b93
Johnnharry88 <harryjohnwhye@gmail.com>
2023-08-30 04:08:48 -0700

    Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts_recursion - program that prints strings
+ * @s: character pointe
+ * Return: (0)
+ */
+void _puts_recursion(char *s)
+{
+	if (s == '\0')
+	{
+		-putchar(s);
+	}
+	_putchar('\n');
+return (0);
+}
