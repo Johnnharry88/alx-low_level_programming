be23934af6fd9f05d0c4bc4bdc61d7a3dd580ffe
Johnnharry88 <harryjohnwhye@gmail.com>
2023-08-30 05:09:46 -0700

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion -program that outputs the length of a string
+ * @s: Character pointer
+ * Return: length
+ */
+int _strlen_recursion(char *s)
+{
+	int lnt;
+
+	lnt = 0;
+	if (*s)
+	{
+		lnt = (lnt + 1);
+		lnt = (lnt + (_strlen_recursion(s + 1));
+	}
+return (lnt);
+}
