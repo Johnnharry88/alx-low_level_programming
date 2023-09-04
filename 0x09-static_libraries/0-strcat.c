eb1dce91a8cdd642f8d4fb2513701c481c64612d
Peace-lov <chidieberepeace42@gmail.com>
2023-09-04 18:13:13 +0100

    Task 0

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *_strcat -progran that catenates strings
+ * @dest: Output pointer
+ * @src: Input pointer
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (dest);
+}
