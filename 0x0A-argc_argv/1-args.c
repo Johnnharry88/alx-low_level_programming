ccc7d255cd8220451bd0cd8a996e5405723233ee
Johnnharry88 <harryjohnwhye@gmail.com>
2023-09-05 14:01:30 +0100

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main -program to be executed
+ * @argc: Argument Counter of Integer Type
+ * @argv: Chsaracter pointer
+ * Return: (0) Success
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%d", argc);
+	}
+}
