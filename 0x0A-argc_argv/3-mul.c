af97e11bea045e10991e3b3e4ec1b3a5708e2c3b
Johnnharry88 <harryjohnwhye@gmail.com>
2023-09-05 21:59:15 +0100

    Task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -16,8 +16,8 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		alx1 = atoi(argv[argc - 2]};
-		alx2 = atoi(argv[argc - 1]};
+		alx1 = atoi(argv[argc - 2]);
+		alx2 = atoi(argv[argc - 1]);
 		final = alx1 * alx2;
 		printf("%d\n", final);
 	}
