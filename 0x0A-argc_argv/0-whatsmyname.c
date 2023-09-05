0a0d18a4d9fff26500e1c717d9e3f7191fe79a28
Johnnharry88 <harryjohnwhye@gmail.com>
2023-09-05 13:46:19 +0100

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
- * print_appname -program to be executed
+ * main -program to be executed
  * @argc: Argument Counter of integer type
  * @argv: String pointer
  * Return: (0) Success
  */
-int print_appname(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	if (argc == 1)
-	_putchar(*arg[0]);
-	_putchar('\n');
-return (0);
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 int _putchar(char c);
-int print_appname(int argc, *argv[]);
+int print_appname(int argc, char *argv[]);
 
 #endif
