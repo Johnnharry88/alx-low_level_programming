bdd3a6f03b2fc30b35bff7d75cd423176df46fe9
Johnnharry88 <harryjohnwhye@gmail.com>
2023-09-06 19:26:21 +0100

    Task 4

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,1 +1,19 @@
-
+#include "main.h"
+/**
+ * free_grid -program that frees an array of two dimension
+ * @grid: Integer pointer parameter
+ * @height: integer parameter
+ * Return: Void
+ */
+void free_grid(int **grid, int height)
+{
+	if (height > 0 && grid != NULL)
+	{
+		whilw (height >= 0)
+		{
+			free(grid[height]);
+			free[grid];
+			height = height - 1;
+		}
+	}
+}
