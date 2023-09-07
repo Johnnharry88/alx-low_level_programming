551e3a27a8185e858a585c58a14f6f40df8962b0
Johnnharry88 <harryjohnwhye@gmail.com>
2023-09-07 13:16:28 +0100

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (sch > 0)
 		sch = n;
 	/* Assigning malloc */
-	school = malloc(sizeof(char) * (alx * sch) + 1);
+	school = malloc(sizeof(char) * (alx * sch + 1));
 	if (school == NULL)
 	return (NULL);
 	for (best = 0; best < alx; best++)
