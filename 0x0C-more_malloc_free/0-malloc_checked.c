f44cf86f5e203257baccf00c787341c851b503c5
Johnnharry88 <harryjohnwhye@gmail.com>
2023-09-07 07:27:56 +0100

    TAsk 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * malloc_checked -program that uses maloc to allocate memory
+ * @b: Input value
+ * Return: A pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *alx;
+
+	alx = malloc[b];
+	if (alx == NULL)
+	exit(980;
+	return (alx);
+}
