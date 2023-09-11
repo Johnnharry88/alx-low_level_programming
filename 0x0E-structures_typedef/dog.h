2df153aff20cec1da7c8ba9c80bbae2004d5e304
Johnnharry88 <harryjohnwhye@gmail.com>
2023-09-11 12:38:12 +0100

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * dog - structure name
+ * @name: Dog's name holder of data type char
+ * @age: Age of dog holder of float type 
+ * @owner: Dog owner holder of data type char
+ * Return: void
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
