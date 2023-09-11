d98fbfac567955304b13691d8ba1aa13a428fbe2
Johnnharry88 <harryjohnwhye@gmail.com>
2023-09-11 15:36:08 +0100

    Task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,50 @@
+#include "dog.h"
+/**
+ * new_dog - programs that defines structure.
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog Owner.
+ * Return: struct dog or NULL
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int rec, n = 0, o = 0;
+	dog_t *alx;
+
+	alx = malloc(sizeof(*alx));
+	if (alx == NULL)
+	free(alx);
+	return (NULL);
+	if ((!(name) || !(owner)))
+	free(alx);
+	return (NULL);
+	for (n = 0; name[n]; n++)
+	{
+		;
+	}
+	for (o = 0; owner[o]; o++)
+	{
+		;
+	}
+	(*alx).name = malloc(n + 1);
+	(*alx).owner = malloc(o + 1);
+	for (rec = 0; rec < n; rec++)
+	{
+		(*alx).name[rec] = name[rec];
+	}
+	(*alx).name[rec] = '\0';
+	(*alx).age = age;
+	for (rec = 0; rec < o; rec++)
+	{
+		(*alx).owner[rec] = owner[rec];
+	}
+	(*alx).owner[rec] = '\0';
+	return (alx);
+	if (!(alx->name) || !(alx->owner))
+	{
+		free(alx->owner);
+		free(alx->name);
+		free(alx);
+		return (NULL);
+	}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,7 +16,9 @@ struct dog
 };
 
 #include <stdio.h>
+#include <stdlib.h>
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
