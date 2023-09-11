8b654f567490e76b910cb39ffbddce0bd6c3589e
Johnnharry88 <harryjohnwhye@gmail.com>
2023-09-11 16:29:10 +0100

    TAsk 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main -porgram that prints the name of a file
+ * Return: (0) Success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
