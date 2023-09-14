68b216fb5cf76e149da637b07999c35557c8c709
Johnnharry88 <harryjohnwhye@gmail.com>
2023-09-14 15:17:41 +0100

    Task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,8 +1,48 @@
 #include "variadic_functions.h"
 /**
- * print_all - program that prints all items to the screen
- * @char: a constant pointer to strings
- * @format: input of strings identifiers
- * Return: (void).
+ * print_all - programs that prints all strings
+ * @format: lsit of type of arguments
+ * Return: void.
  */
+void print_all(const char *const format, ...)
+{
+	char *alpha, *div = "";
+	va_list items;
+	int alx = 0;
 
+	va_start(items, format);
+	if (format)
+	{
+		while (format[alx])
+		{
+			switch (format[alx])
+			{
+				case '1':
+					printf("%s%d", div, va_arg(items, int));
+					break;
+				case 'c':
+					printf("%s%c", div,  va_arg(items, int));
+					break;
+				case 'f':
+					printf("%s%f", div, va_arg(items, double));
+					break;
+				case 's':
+					alpha = va_arg(items, char*);
+					if (!alpha)
+					{
+						printf("(nil)");
+						break;
+					}
+					printf("%s%s", div, alpha);
+					break;
+				default:
+					alx = alx + 1;
+					continue;
+			}
+			div = ", ";
+			alx = alx + 1;
+		}
+	}
+printf("\n");
+va_end(items);
+}
