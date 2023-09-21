ddea8ea2b236e45a1ca869ce63c85ae1441b391d
Johnnharry88 <harryjohnwhye@gmail.com>
2023-09-21 21:45:48 +0100

    Task 0

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_list - function that frees a singly linked list.
+ * @head: A pointer to list
+ * Return: void.
+ */
+void free_list(list_t *head)
+{
+	list_t *ram;
+
+	ram = head;
+	while (head != NULL)
+	{
+		ram = head;
+		head = head->next;
+		free(ram->str);
+		free(rram);
+	}
+	free(head);
+}
