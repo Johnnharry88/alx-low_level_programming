37ee59952bb2746f2bb07877d78d04a9064d03d6
Johnnharry88 <harryjohnwhye@gmail.com>
2023-09-21 22:10:29 +0100

    Task 0

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * front_main - a function that prnts a string  before main.
+ * function execution
+ * Retiurn: void.
+ */
+void front_main(void)
+{
+	printf("You' re beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
