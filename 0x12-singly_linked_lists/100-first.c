ba307e83e1ca98b2b6c921818ed1fff4da804db2
Johnnharry88 <harryjohnwhye@gmail.com>
2023-09-22 11:36:55 +0100

    Task 0

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,6 +9,6 @@ void front_main(void)__attribute__((constructor));
  */
 void front_main(void)
 {
-	printf("You' re beat! and yet, you must allow,\n");
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
