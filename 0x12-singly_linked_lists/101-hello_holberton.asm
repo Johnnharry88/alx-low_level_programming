31944bf826b813a491dd4d1f2828378ed3ca6226
Johnnharry88 <harryjohnwhye@gmail.com>
2023-09-22 11:48:32 +0100

    TAsk 0

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,13 @@
+SECTION .data
+	msg: do "Hello, Holberton", 0ah
+	.text
+	global main
+	main:
+		mov edx, 17 
+		mov ecx, msg
+		mov, ebx, 1
+		mov, eax, 4
+		int 80h
+		mov, ebx, 0
+		mov eax, 1
+		int 80h
