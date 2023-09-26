9cda731ec1be036f4e03859db347628ab43f12d3
Johnnharry88 <harryjohnwhye@gmail.com>
2023-09-26 09:13:41 +0100

    Task 0

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -6,12 +6,12 @@
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t alx = 1;
+	size_t alx = 0;
 
 	while (h != NULL)
 	{
 		h = h->next;
-		alx = alx + 1;
+		alx = alx + l;
 	}
 	return (alx);
 }
