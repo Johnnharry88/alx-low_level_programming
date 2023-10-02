f78d572d576c5a4f4ec418fee46db7d980ccdefd
Johnnharry88 <harryjohnwhye@gmail.com>
2023-10-02 16:16:44 +0100

    Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * real_testfile - reads a file in text format and prints letter
+ * read_textfile - reads a file in text format and prints letter
  * @filename: Name of file.
  * @letters: letter number printed.
  * Return: no of letter printed or 0.
  */
-size_t real_testfile(const char *filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int alx;
 	size_t input, out;
@@ -16,7 +16,7 @@ size_t real_testfile(const char *filename, size_t letters)
 	alx = open(filename, O_RDONLY);
 	if (alx == -1)
 		return (0);
-	store = malloc(sizeof(char) * (letters));
+	store = malloc(letters);
 	if (!store)
 		return (0);
 	input = read(alx, store, letters);
