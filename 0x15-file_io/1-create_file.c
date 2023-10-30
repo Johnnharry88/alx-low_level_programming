6aab2ea2fb0471138b7e3dad5e2e54f4c1f1a284
Johnnharry88 <harryjohnwhye@gmail.com>
2023-10-30 23:40:28 +0100

    task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@ int create_file(const char *filename, char *text_content)
 	int action, rec;
 	int output;
 
-	if (filename == NULL || text_content == -1)
+	if (filename == NULL || text_content == NULL)
 		return (-1);
 	action = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	
