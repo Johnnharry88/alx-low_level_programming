99bf0d4254550935a3b64633e8e9b26732604510
Johnnharry88 <harryjohnwhye@gmail.com>
2023-10-30 23:22:07 +0100

    TAsk

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -30,19 +30,27 @@ int main(int argc, char *argv[])
 {
 	int file_to;
 	int file_from;
-	int errorcheck;
 	int alx;
 	int wrt;
 	char store[1024];
 
-	if (argc < 3 || argc > 3)
+	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
 	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(98);
+	}
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_APPEND, 0664);
-	check_error(file_from, file_to, argv);
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %d\n", file_from);
+		exit(99);
+	}
 	alx = 1024;
 	while (alx == 1024)
 	{
@@ -53,8 +61,7 @@ int main(int argc, char *argv[])
 		if (wrt == -1)
 			check_error(0, -1, argv);
 	}
-	errorcheck = close(file_to);
-	if (errorcheck == -1)
+	if (close(file_from) == -1 || close(file_to) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
