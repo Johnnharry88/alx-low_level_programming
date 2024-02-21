02713ad605b08a049de709b35bdff1b96498be2a
Johnnharry88 <harryjohnwhye@gmail.com>
2024-02-21 23:49:50 +0100

    MakeFile

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,16 +3,15 @@
 
 def island_perimeter(grid):
     x = 0
-    lent = len(grid) 
+    lent = len(grid) - 1 
     for i in range(lent):
         for j in range(len(grid[i])):
-            if n == 1:
-                if i <= 0 or not grid[i - 1][j]:
-                    x = x + 1
-                if j <= 0 or not grid[i][j - 1]:
-                    x = x + 1
-                if j >= len(grid[i]) or not grid[i][j + 1]:
-                    x = x + 1
-                if i >= lent or not grid[i + 1][j]:
-                    x = x + 1
+            if i <= 0 or not grid[i - 1][j]:
+                x = x + 1
+            if j <= 0 or not grid[i][j - 1]:
+                x = x + 1
+            if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
+                x = x + 1
+            if i >= lent or not grid[i + 1][j]:
+                x = x + 1
     return x
